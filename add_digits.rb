110728472f78db6c611ba69704f76c015029268c
maysonet <maysonet@Christophers-MacBook-Pro.local>
2015-06-09 10:35:01 -0400

    Added variable for input file

--- add_digits.rb
+++ add_digits.rb
@@ -1,12 +1,16 @@
 #Sum all the digits of all numbers in the file
 numbers = []
-File.readlines("numbers.txt").each do |number|
-number = number.to_i
+input_file = File.open("numbers.txt")
+File.readlines(input_file).each do |number|
+	number = number.to_i
+
 	while number > 0
 		digit = number % 10
 		number /= 10
 		numbers.push(digit) #add digit to array of numbers
 	end
+
 end
+input_file.close()
 
 puts numbers.inject(:+) #sum the numbers in the array
